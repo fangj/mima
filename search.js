f7cc60a596ff9fb6f3edcdb09a05779fb1e5115a
Fang Jian <fangjianmail@126.com>
2015-09-17 17:32:48 +0800

    remove unused poet

--- search.js
+++ search.js
@@ -3,19 +3,9 @@ var Sequelize=require('sequelize');
 var sequelize = new Sequelize('sqlite://tangall.sqlite',{logging: false});
 var Poetries = sequelize.define('poetries', {
     id: {field: 'id',type: Sequelize.INTEGER, primaryKey: true,uniqueKey:true },
-    poet_id:Sequelize.INTEGER,
     poet:Sequelize.STRING,
     title:Sequelize.STRING,
     content:Sequelize.STRING,
-}, {
-  freezeTableName: true, // Model tableName will be the same as the model name
-  createdAt   : 'created_at',
-  updatedAt   : 'updated_at',
-});
-
-var Poet=sequelize.define('poets', {
-    id: {field: 'id',type: Sequelize.INTEGER, primaryKey: true,uniqueKey:true },
-    name:Sequelize.STRING
 }, {
   freezeTableName: true, // Model tableName will be the same as the model name
   createdAt   : 'created_at',
