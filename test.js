af44adad9b8d5c5bccdb998903c1097861226ce3
Fang Jian <fangjianmail@126.com>
2015-09-17 20:50:26 +0800

    fix empty str

--- test.js
+++ test.js
@@ -8,21 +8,23 @@ var mima=require('./mima');
 //mima.mima();
 // console.log(mima.translateNumber('我们贰啦啦啦'));
 // console.log(replaceAll('贰',2,'我们贰啦啦啦'));
-// mima.mima('贰').then(function(poetry){
-// 	console.log(poetry);
-// });
+
+mima.mima('贰').then(function(poetry){
+	console.log(poetry);
+});
+
 // mima.mima().then(function(poetry){
 // 	console.log(poetry);
 // });
 
-for (var i = 0; i < 1000; i++) {
-	var p=search.getRandomPoetry();
-	p.then(searchEmptyPoetry);
-}
+// for (var i = 0; i < 1000; i++) {
+// 	var p=search.getRandomPoetry();
+// 	p.then(searchEmptyPoetry);
+// }
 
-function searchEmptyPoetry(poetry){
-		if(!poetry){console.log('found');}
-}
+// function searchEmptyPoetry(poetry){
+// 		if(!poetry){console.log('found');}
+// }
 
 // for (var i = 0; i < 100; i++) {
 // 	var p=mima.mima('贰');
