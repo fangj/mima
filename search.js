b34969b2caaa398af81322a506362d9428f4e9f7
Fang Jian <fangjianmail@126.com>
2015-09-17 14:34:19 +0800

    poet ok

--- search.js
+++ search.js
@@ -4,7 +4,6 @@ var sequelize = new Sequelize('sqlite://tangall.sqlite');
 var Poetries = sequelize.define('poetries', {
     id: {field: 'id',type: Sequelize.INTEGER, primaryKey: true,uniqueKey:true },
     poet_id:Sequelize.INTEGER,
-    content:Sequelize.STRING,
     title:Sequelize.STRING,
     content:Sequelize.STRING,
 }, {
@@ -13,10 +12,23 @@ var Poetries = sequelize.define('poetries', {
   updatedAt   : 'updated_at',
 });
 
-function findOne(){
-  return Poetries.findOne({ where: {id: 1} });
-}
+
+var Poet=sequelize.define('poets', {
+    id: {field: 'id',type: Sequelize.INTEGER, primaryKey: true,uniqueKey:true },
+    name:Sequelize.STRING
+}, {
+  freezeTableName: true, // Model tableName will be the same as the model name
+  createdAt   : 'created_at',
+  updatedAt   : 'updated_at',
+});
+
 
 Poetries.findOne().then(function(poetry){
 	console.log(poetry.get({plain:true}));
-})
+});
+
+Poet.findOne().then(function(poet){
+	console.log(poet.get({plain:true}));
+});
+
+
