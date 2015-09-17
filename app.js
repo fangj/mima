0246b1b064eb699222198968fac3bd3e832bc300
Fang Jian <fangjianmail@126.com>
2015-09-17 21:48:54 +0800

    static index.html

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//app.use('/', routes);
+//app.use('/users', users);
 app.use('/mima',mima);
 
 // catch 404 and forward to error handler
