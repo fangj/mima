73d85bdb2999fb3416585fc6d4d9aaa4c71f53ec
Fang Jian <fangjianmail@126.com>
2015-09-17 21:32:10 +0800

    web ok

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var mima=require('./mima/index');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/mima',mima);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
